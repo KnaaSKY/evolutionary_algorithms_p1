1c7b9919bd32111130db18e8b64fa0cc31167bf1
no one cares <smart2015den@gmail.com>
2024-01-13 18:26:57 +0100

    selection_new

--- Selection/selection.py
+++ Selection/selection.py
@@ -13,56 +13,30 @@ class Selection:
         self.min_method = min_method
         self.individuals_best_amount = individuals_best_amount
 
-    def selection(self, population: Population) -> dict:
-        population_gen = []
-        population_y = []
-        for i in range(population.individual_amount):
-            population_y.append(population.individuals[i].chromosome.actual_value_y)
-
-        for j in range(population.individual_amount):
-            population_gen.append(population.individuals[j].chromosome.gene)
-
-        pop_dict = dict()
-        for k in range(population.individual_amount):
-            pop_dict[tuple(population_gen[k])] = population_y[k]
-
+    def selection(self, population: Population) -> list[Individual]:
         match self.selection_type:
             case Selection.selection_type_best:
-                old_gens = self.selection_best_method(pop_dict)
-                # for element in old_gens:
-                #     print(element, end='\n')
-                return old_gens
+                best_chromosomes = self.selection_best_method(population)
+                return best_chromosomes
             case Selection.selection_type_roulette:
                 print("selection_type_roulette")
             case Selection.selection_type_tournament:
                 print("selection_type_tournament")
 
-    def selection_best_method(self, pop_dict: dict) -> dict:
+    def selection_best_method(self, population: Population) -> list[Individual]:
 
         if self.min_method == Selection.evaluation_min:
-            pop_dict_sorted = dict(sorted(pop_dict.items(), key=lambda item: item[1]))
-            # for key, value in pop_dict_sorted.items():
-            #     print(f"{key}: {value}")
-            best_gen = {}
-            for key, value in pop_dict_sorted.items():
-                best_gen[key] = value
-                if len(best_gen) == self.individuals_best_amount:
-                    break
-            # for element in best_gen:
-            #     print(element, end='\n')
-            return best_gen
+            individuals_sorted = sorted(population.individuals, key=lambda x: x.fitness_function_value)
+            individuals_list = []
+            for i in range(0, self.individuals_best_amount):
+                individuals_list.append(individuals_sorted[i])
+            return individuals_list
         else:
-            pop_dict_sorted = dict(sorted(pop_dict.items(), key=lambda item: item[1], reverse=True))
-            # for key, value in pop_dict_sorted.items():
-            #     print(f"{key}: {value}")
-            best_gen = {}
-            for key, value in pop_dict_sorted.items():
-                best_gen[key] = value
-                if len(best_gen) == self.individuals_best_amount:
-                    break
-            # for element in best_gen:
-            #     print(element, end='\n')
-            return best_gen
+            individuals_sorted = sorted(population.individuals, key=lambda x: x.fitness_function_value, reverse=True)
+            individuals_list = []
+            for i in range(0, self.individuals_best_amount):
+                individuals_list.append(individuals_sorted[i])
+            return individuals_list
 
 
     def selection_roulette_method(self):
--- main.py
+++ main.py
@@ -5,39 +5,46 @@ from Inversion.inversion import *
 a: float = -10
 b: float = 10
 power_number_intervals: int = 6  # 10^power_number_intervals - number of intervals to divide our range
-individual_amount: int = 10
-individuals_best_amount: int = 10
+individual_amount: int = 5
+variable_amount = 1
+individuals_best_amount: int = 3
 epochs_amount: int = 0
 Chromosome.size = chromosome_length(a, b, power_number_intervals)
+Individual.range_start = a
+Individual.range_end = b
 
-population = population_init(individual_amount, a, b)
+population = population_init(individual_amount, variable_amount)
 
 
 for i in range(0, individual_amount):
     print()
     population.individuals[i].display()
 
+
 selekcja = Selection(1, True, individuals_best_amount)
+best_gens = selekcja.selection(population)
+for i in range(0, 3):
+    for j in range(0, variable_amount):
+        print(best_gens[i].chromosome[j].gene, end="    ")
+        print(best_gens[i].chromosome_values[j])
 
 
-old_gen = selekcja.selection(population)
 
-print('\n'*5)
-
-population = population_create(individual_amount, old_gen, a, b)
-for i in range(0, individuals_best_amount):
-    print()
-    population.individuals[i].display()
+# population = population_create(individual_amount, old_gen, a, b)
+# for i in range(0, individuals_best_amount):
+#     print()
+#     population.individuals[i].display()
 
 
-inversion = Inversion(0.5)
-inversion.inversion(population)
-calculate_gene_values(population, a, b)
-for i in range(0, individuals_best_amount):
-    print()
-    population.individuals[i].display()
+# inversion = Inversion(0.5)
+# inversion.inversion(population)
+# calculate_gene_values(population, a, b)
+# for i in range(0, individuals_best_amount):
+#     print()
+#     population.individuals[i].display()
 
 
 
 print("DANYLO")
+
 
