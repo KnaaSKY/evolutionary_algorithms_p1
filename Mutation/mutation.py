f421c2c042515a79af85d6703f87434f4f5d687f
Arkadiusz Knapik <arkadiusz.knapik.200@gmail.com>
2024-01-14 01:33:14 +0100

    mutation

--- Mutation/mutation.py
+++ Mutation/mutation.py
@@ -11,50 +11,50 @@ class Mutation:
         self.mutation_type = mutation_type
         self.probability = probability
 
-    def mutation(self, population: Population):
+    def mutation(self, population: Population, individual_amount_no_elitism: int):
 
         match self.mutation_type:
             case Mutation.mutation_type_one_point:
-                self.mutation_one_point_method(population)
+                self.mutation_one_point_method(population, individual_amount_no_elitism)
             case Mutation.mutation_type_two_points:
-                self.mutation_two_points_method(population)
+                self.mutation_two_points_method(population, individual_amount_no_elitism)
             case Mutation.mutation_type_edge:
-                self.mutation_edge_method(population)
+                self.mutation_edge_method(population, individual_amount_no_elitism)
 
-    def mutation_one_point_method(self, population: Population):
-        length = population.individuals[0].chromosome.size
-        for i in range(population.individual_amount):
+    def mutation_one_point_method(self, population: Population, individual_amount_no_elitism: int):
+        length = population.individuals[0].chromosome[0].size
+        for i in range(individual_amount_no_elitism):
             chance_mutation = round(random.uniform(0, 1), 2)
             if 0 <= chance_mutation <= self.probability:
                 index_change = random.randint(0, length - 1)
-                population.individuals[i].chromosome.gene[index_change] = 0 if (
-                        population.individuals[i].chromosome.gene[index_change] == 1) else 1
-            elif self.probability < chance_mutation <= 1:
-                continue
+                print(f"gen: {i} index: {index_change}")
+                for j in range(population.variables_amount):
+                    population.individuals[i].chromosome[j].gene[index_change] = 0 if (
+                        population.individuals[i].chromosome[j].gene[index_change] == 1) else 1
 
-    def mutation_two_points_method(self, population: Population):
-        length = population.individuals[0].chromosome.size
-        for i in range(population.individual_amount):
+    def mutation_two_points_method(self, population: Population, individual_amount_no_elitism: int):
+        length = population.individuals[0].chromosome[0].size
+        for i in range(individual_amount_no_elitism):
             chance_mutation = round(random.uniform(0, 1), 2)
             if 0 <= chance_mutation <= self.probability:
                 index_change = random.randint(0, length - 1)
                 index2_change = random.randint(0, length - 1)
                 while index_change == index2_change:
                     index2_change = random.randint(0, length - 1)
-                population.individuals[i].chromosome.gene[index_change] = 0 if (
-                        population.individuals[i].chromosome.gene[index_change] == 1) else 1
-                population.individuals[i].chromosome.gene[index2_change] = 0 if (
-                        population.individuals[i].chromosome.gene[index2_change] == 1) else 1
-            elif self.probability < chance_mutation <= 1:
-                continue
+                print(f"gen: {i} index1: {index_change} index2: {index2_change}")
+                for j in range(population.variables_amount):
+                    population.individuals[i].chromosome[j].gene[index_change] = 0 if (
+                            population.individuals[i].chromosome[j].gene[index_change] == 1) else 1
+                    population.individuals[i].chromosome[j].gene[index2_change] = 0 if (
+                            population.individuals[i].chromosome[j].gene[index2_change] == 1) else 1
 
-    def mutation_edge_method(self, population: Population):
-        length = population.individuals[0].chromosome.size
-        for i in range(population.individual_amount):
+    def mutation_edge_method(self, population: Population, individual_amount_no_elitism: int):
+        length = population.individuals[0].chromosome[0].size
+        for i in range(individual_amount_no_elitism):
             chance_mutation = round(random.uniform(0, 1), 2)
             if 0 <= chance_mutation <= self.probability:
                 index_change = random.choice([0, length-1])
-                population.individuals[i].chromosome.gene[index_change] = 0 if (
-                        population.individuals[i].chromosome.gene[index_change] == 1) else 1
-            elif self.probability < chance_mutation <= 1:
-                continue
+                print(f"gen: {i} index: {index_change}")
+                for j in range(population.variables_amount):
+                    population.individuals[i].chromosome[j].gene[index_change] = 0 if (
+                            population.individuals[i].chromosome[j].gene[index_change] == 1) else 1
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from functions import *
 from Selection.selection import *
 from Crossover.crossover import *
 from Inversion.inversion import *
+from Mutation.mutation import *
 
 a: float = -10
 b: float = 10
@@ -18,11 +19,9 @@ Individual.range_end = b
 
 population = population_init(individual_amount, variable_amount)
 
-
 for i in range(0, individual_amount):
     print()
     population.individuals[i].display()
-
 
 selekcja = Selection(1, True, individual_selection_amount)
 best_gens = selekcja.selection(population)
@@ -35,15 +34,23 @@ crossover = Crossover(2, 0.7, individual_amount, individual_elitism_amount)
 population = crossover.crossover(best_gens)
 
 for i in range(0, individual_amount_no_elitism):
-    print()
-    population.individuals[i].display()
+    for j in range(0, variable_amount):
+        print(population.individuals[i].chromosome[j].gene, end="\n")
 
-inversion = Inversion(0.7)
-inversion.inversion(population, individual_amount_no_elitism)
+
+mutation = Mutation(1, 0.3)
+mutation.mutation(population, individual_amount_no_elitism)
 for i in range(0, individual_amount_no_elitism):
-    print()
-    population.individuals[i].display()
+    for j in range(0, variable_amount):
+        print(population.individuals[i].chromosome[j].gene, end="\n")
 
+# inversion = Inversion(0.7)
+# inversion.inversion(population, individual_amount_no_elitism)
+# for i in range(0, individual_amount_no_elitism):
+#     print()
+#     population.individuals[i].display()
+#
+# for i in range(0, individual_amount_no_elitism):
+#     print()
+#     population.individuals[i].display()
 print("DANYLO")
-
-
